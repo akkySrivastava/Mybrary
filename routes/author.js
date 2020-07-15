cacd3d4f68a737e9d542d2a9e92bddcc50b133d4
Akky6srivastava <as7750307@gmail.com>
2020-07-15 18:27:42 +0530

    Intial commit

--- routes/author.js
+++ routes/author.js
@@ -1,19 +1,62 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/authors')
 
 //All Authors Route
-router.get('/', (req,res) => {
-    res.render('author/index')
+router.get('/', async (req,res) => {
+    const searchOptions = {}
+    if(req.query.name != null && req.query.name !== '')
+    {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+            const authors = await Author.find(searchOptions)
+            res.render('authors/index',{
+                authors: authors,
+                searchOptions: req.query
+            })
+    }
+    catch{
+            res.redirect('/')
+    }
+    
 })
 
 
 //New Authoer Router
 router.get('/new',(req,res) =>{
-    res.render('author/new')
+    res.render('authors/new', {author: new Author()})
 })
 
 //create Author Route
-router.post('/',(req,res) =>{
-    res.send('Create')
+router.post('/', async (req,res) =>{
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+            const newAuthor = await author.save()
+            //res.redirect(`authors/${newAuthor.id}`)
+            res.redirect(`authors`)
+    }
+    catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+    /*author.save((err,newAuthor) => {
+        if(err)
+        {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        }
+        else{
+            //res.redirect(`authors/${newAuthor.id}`)
+            res.redirect(`authors`)
+        }
+    })
+    //res.send(req.body.name)*/
 })
 module.exports = router
--- server.js
+++ server.js
@@ -9,15 +9,17 @@ const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/author')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit : '10mb' , extended: false}))
 
 app.use('/', indexRouter)
-app.use('/author',authorRouter)
+app.use('/authors',authorRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
