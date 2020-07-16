b9dff4c6e90053165de1fbef5b283e44f8c31c3a
Akky6srivastava <as7750307@gmail.com>
2020-07-16 09:09:11 +0530

    Books Finished

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ if(process.env.NODE_ENV !=='production')
 const express = require('express');
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/author')
-const bodyParser = require('body-parser')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({ limit : '10mb' , extended: false}))
 
 app.use('/', indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books', bookRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
